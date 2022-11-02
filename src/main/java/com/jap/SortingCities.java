44bcf90174c9b33876f141397ee800e5941cab10
naveen59835 <naveenkumar59835@gmail.com>
2022-11-02 16:54:49 +0530

    done

--- src/main/java/com/jap/SortingCities.java
+++ src/main/java/com/jap/SortingCities.java
@@ -1,13 +1,92 @@
 package com.jap;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
- *
  */
-public class SortingCities
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class SortingCities {
+    public static void main(String[] args) {
+        SortingCities sortingCities = new SortingCities();
+        int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+        String[] cityNames = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+
+
+        int[] kilometers = sortingCities.citySorting(distance);
+        for (int kilometer : distance) {
+            System.out.println("Hi Caroline the shortest distance From Zurich is sorted as " + kilometer);
+
+
+        }
+        String sortedAlphabet = sortingCities.convertToUpperCase(Arrays.toString(cityNames));
+        System.out.println("Hi caroline the Sorted alphabet is " + sortedAlphabet);
+        int km1 = distance.length;
+
+
+        System.out.println("Hi caroline the city which is 270 kms from Zurich is " + sortingCities.toFindTheCity(cityNames, distance) + " " + "kms is".concat(cityNames[5]));
+        System.out.println("Hi caroline the city which is 52 kms from Zurich is " + sortingCities.tofindTheCityAbove(cityNames, distance) + " " + "kms is".concat(cityNames[1]));
+
+
     }
+
+    public int[] citySorting(int[] kms) {
+        int temp;
+
+        for (int pass = 0; pass < kms.length; pass++) {
+            for (int index = 0; index < kms.length - 1; index++) {
+                if (kms[index] > kms[index + 1]) {
+                    temp = kms[index + 1];
+                    kms[index + 1] = kms[index];
+                    kms[index] = temp;
+
+                }
+
+            }
+
+        }
+        return kms;
+
+
+    }
+
+    public String convertToUpperCase(String cityNames) {
+        String names = cityNames.toUpperCase();
+        String upper = names.toUpperCase();
+        return upper;
+    }
+
+    public Object toFindTheCity(String[] cityNames, int[] distance) {
+        String kilometers = "270";
+        int[] distanceOfCity = new int[distance.length];
+        for (int index = 0; index < distance.length; index++) {
+            distanceOfCity[index] = Integer.valueOf((String.valueOf(distance[index])));
+            if (distanceOfCity[index] >= 270) {
+                return distance[index];
+            }
+
+        }
+        return null;
+    }
+
+    public Object tofindTheCityAbove(String[] cityNames, int[] distance) {
+        String kilometers = "50";
+        int[] distanceOfCity = new int[distance.length];
+        for (int index = 0; index < distance.length; index++) {
+            distanceOfCity[index] = Integer.valueOf((String.valueOf(distance[index])));
+            if (distanceOfCity[index] >= 50) {
+                return distance[index];
+            }
+
+        }
+        return null;
+    }
+
+
 }
+
+
+
+
+
+
+
--- src/test/java/com/jap/SortingCitiesTest.java
+++ src/test/java/com/jap/SortingCitiesTest.java
@@ -2,18 +2,28 @@ package com.jap;
 
 import org.junit.After;
 import org.junit.Before;
-
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class SortingCitiesTest {
+    SortingCities sortingCities;
+
 
     @Before
-    public void setUp()  {
+    public void setUp() {
+        sortingCities = new SortingCities();
+        int[] distance = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+        String[] cityNames = {"Bern", "Lucerne", "Interlaken", "Grindelwald", "Engelberg", "Geneva", "Murren", "Basel", "Zermatt", "Jungfraujoch"};
+
     }
 
     @After
-    public void tearDown()  {
+    public void tearDown() {
     }
 
-    //write all the test cases here
+    @Test
+    public void citySorting() {
+        int[] distances = {138, 52, 118, 136, 85, 276, 103, 87, 214, 101};
+
+    }
+//write all the test cases here
 }
